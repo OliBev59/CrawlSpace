0a155ef58d38402922c896c8230b049e2cce252f
Kian-SBG <113431242+Kian-SBG@users.noreply.github.com>
2022-10-03 14:08:05 +0100

    Update main.js

--- map/main.js
+++ map/main.js
@@ -1,8 +1,6 @@
 //Import stuff from openmap api
 
 
-import Circle from 'ol/geom/Circle';
-import * as olColor from 'ol/color';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import {Map, View} from 'ol';
@@ -10,24 +8,9 @@ import {fromLonLat} from 'ol/proj';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Fill, Stroke, Style} from 'ol/style';
 
 
-const image = new CircleStyle({
-  radius: 5,
-  //fill: 'red',
-  stroke: new Stroke({color: 'red', width: 1}),
-});
-
-const styles = {
-  'Point': new Style({
-    image: image,
-  }),
-};
-
-const styleFunction = function (feature) {
-  return styles[feature.getGeometry().getType()];
-};
 
 const vectorSource = new VectorSource ({ //database as object
   format: new GeoJSON(),
@@ -35,7 +18,10 @@ const vectorSource = new VectorSource ({ //database as object
 });
 const vectorLayer = new VectorLayer({
   source: vectorSource,
-  style: styleFunction,
+  style: {
+    'circle-radius': 5,
+    'circle-fill-color': 'red'
+  },
 });
 
 

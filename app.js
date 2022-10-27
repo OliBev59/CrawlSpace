4985ae2089ed1d67245cd06c449e3866b7dc46bb
vishwapatel17 <vishwa.patel@skybettingandgaming.com>
2022-10-27 11:31:36 +0100

    editing comments

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express();
 
 const routes = require("./routes/routes");
 
-//comment
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -15,28 +13,11 @@ app.use(express.static("./public")); // index.html
 
 app.use("/", routes);
 
-// remove for sample files
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!");
   });
 
-
-
-
-  
-
-  
 app.listen(3000);
-
-  
-
-// making sure its working
-// app.use((req, resp) => {
-//     console.info("middleware called");
-//     // resp.send("Middleware called");
-// });
-
-
 
 console.log("This is working");
 

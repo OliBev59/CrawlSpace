4cdd5db21b503ec25ae331ac4ad293c38064c3f7
Kian-SBG <kian.popat@skybettingandgaming.com>
2022-11-30 15:06:55 +0000

    cookie do stuff

--- public/cookie.js
+++ public/cookie.js
@@ -1,10 +1,13 @@
 var today = new Date();
 var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
 
-function setCookie(name, value)
-{
-  document.cookie=name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
 function putCookie(form){            //this should set the UserName cookie to the proper value;
  console.log("cookies working")
  setCookie("userName", form[0].username.value);
@@ -13,4 +16,27 @@ function putCookie(form){            //this should set the UserName cookie to th
 }
 function eraseCookie(){
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+}
+function checkCookie() {
+  let user = document.cookie
+  print(user)
+  if ( user !== "" ) {
+  } else {
+    alert("not signed in");
+    window.location.replace("http://localhost:3000/login");
+  }
 }
